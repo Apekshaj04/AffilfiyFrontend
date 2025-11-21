db086daf927cd7eb78583bd8b2f97ece18a1c0bb
Apeksha Jamjar <117656894+Apekshaj04@users.noreply.github.com>
2025-11-21 12:23:51 +0530

    Update Auth.js

--- src/app/Signup/Auth.js
+++ src/app/Signup/Auth.js
@@ -49,7 +49,7 @@ export default function Auth() {
     try {
       setLoading(true);
    const endpoint = isLogin ? "login" : "register";
-const url = `http://54.147.55.29:8080/api/${userType}/${endpoint}`;
+const url = `http://54.197.48.44:8080/api/${userType}/${endpoint}`;
 
 const response = await axios.post(url, { walletAddress, ...formData });
 
